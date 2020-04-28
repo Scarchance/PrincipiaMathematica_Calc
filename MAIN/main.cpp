248d66e46d2b9f384bf67a58086f695fcd36bfd7
Scarchance <yashsirisena@icloud.com>
2020-04-28 08:14:34 -0700

    Principia Mathematica_Calc 1.0

---
+++ MAIN/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+int y;
+int q;
+int t;
+
+int main () {
+    cout << "PRINCIPIA 1.0 LAUNCH SUCCESSFUL" << endl;
+    cout<< "Enter 1st number:";
+    cin>>y ;
+    cout<< "Enter 2st number:";
+    cin>>q ;
+    cout << "Select one of the following operations: add(+), Substract(-), Multiply(*), Divide (/) : " ;
+    char input;
+    cin >>input;
+    if (input=='+') {
+    cout << y+q;
+    }
+    if (input=='-'){
+    cout<< y-q;
+    }
+    if (input=='/') {
+        cout << y/q;
+    }
+    if (input=='*'){
+        cout<< y*q;
+    }
+
+     else {
+         cout << "\n" << "READY FOR NEXT OPERATION";
+        t = 0;
+        while (t == 0) {
+            cout << "\n" << "Enter 1st number:";
+            cin >> y;
+            cout << "Enter 2st number:";
+            cin >> q;
+            cout << "Select one of the following operations: add(+), Substract(-), Multiply(*), Divide (/) : ";
+            char input;
+            cin >> input;
+            if (input == '+') {
+                cout << y + q;
+            }
+            if (input == '-') {
+                cout << y - q;
+                return 0;
+            }
+            if (input == '/') {
+                cout << y / q;
+            }
+            if (input == '*') {
+                cout << y * q;
+            }
+
+        }
+    }
+    return 0;
+}
