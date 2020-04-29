a7abbcdd2a52a32bd7260820aca4a40211823b93
Tharindu Ranaweera <earwax28@gmail.com>
2020-04-29 14:07:06 +0530

    Optimised logical flow

--- MAIN/main.cpp
+++ MAIN/main.cpp
@@ -7,53 +7,36 @@ int q;
 int t;
 
 int main () {
+
     cout << "PRINCIPIA 1.0 LAUNCH SUCCESSFUL" << endl;
-    cout<< "Enter 1st number:";
-    cin>>y ;
-    cout<< "Enter 2st number:";
-    cin>>q ;
-    cout << "Select one of the following operations: add(+), Substract(-), Multiply(*), Divide (/) : " ;
-    char input;
-    cin >>input;
-    if (input=='+') {
-    cout << y+q;
-    }
-    else if (input=='-'){
-    cout<< y-q;
-    }
-    else if (input=='/') {
-        cout << y/q;
-    }
-    else if (input=='*'){
-        cout<< y*q;
-    }
 
-    else {
-        cout << "\n" << "READY FOR NEXT OPERATION";
-        t = 0;
-        while (t == 0) {
-            cout << "\n" << "Enter 1st number:";
-            cin >> y;
-            cout << "Enter 2st number:";
-            cin >> q;
-            cout << "Select one of the following operations: add(+), Substract(-), Multiply(*), Divide (/) : ";
-            char input;
-            cin >> input;
-            if (input == '+') {
-                cout << y + q;
-            }
-            if (input == '-') {
-                cout << y - q;
-                return 0;
-            }
-            if (input == '/') {
-                cout << y / q;
-            }
-            if (input == '*') {
-                cout << y * q;
-            }
+    cout << "\n" << "READY FOR NEXT OPERATION";
+    t = 0;
+    while (t == 0) {
 
+        cout << "\n" << "Enter 1st number:";
+        cin >> y;
+        cout << "Enter 2st number:";
+        cin >> q;
+        cout << "Select one of the following operations: add(+), Substract(-), Multiply(*), Divide (/) or q to Quit : ";
+
+        char input;
+        cin >> input;
+        if (input == '+') {
+            cout << y + q;
+        } else if (input == '-') {
+            cout << y - q;
+        } else if (input == '/') {
+            cout << y / q;
+        } else if (input == '*') {
+            cout << y * q;
+        } else if (input == 'q') {
+            cout << "Exiting! Thanks for using our calculator!"
+            t=1;
+        } else {
+            cout << "Wrong/ unsupported operation, try again";
         }
+
     }
     return 0;
 }
